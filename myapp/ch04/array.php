735ab30c6cab05b3fb0d444e0914bb2d09896069
Rjini <6jin26@gmail.com>
2022-09-27 23:37:15 +0900

    20220927_php_study

---
+++ myapp/ch04/array.php
@@ -0,0 +1,17 @@
+<?php
+$array = array("사과", 5, "바나나", 1, 3.14, "수박");
+echo "<pre>";
+print_r($array);
+echo "</pre>";
+
+$array = ["사과", 5, "바나나", 1, 3.14, "수박"];
+echo "<pre>";
+print_r($array);
+echo "</pre>";
+
+$a = array("가", "나", "다", "라");
+
+echo $a[0]. "<br/>";
+echo $a[1]. "<br/>";
+echo $a[2]. "<br/>";
+echo $a[3]. "<br/>";
