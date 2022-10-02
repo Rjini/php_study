2fb84b9aa9d592a6a80c521c5a1a9b6a80a7e2e0
Rjini <6jin26@gmail.com>
2022-10-03 00:15:52 +0900

    20221003_php_study

---
+++ myapp/ch04/lambda_function3.php
@@ -0,0 +1,17 @@
+<?php
+$sep = ', ';
+
+function myFunc()
+{
+	global $sep;
+	$wor = 'world';
+	
+	return function ($hel) use ($sep, $wor) {
+		$exc = '!';
+		return $hel . $sep . $wor . $exc;
+	};
+}
+
+$hello = myFunc();
+
+echo $hello('Hello');
